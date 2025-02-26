56f47cddca5d45d91f9d7aa1f97c31d9990db1bd
Rupali Kumari <mrupali1394@gmail.com>
2025-02-26 13:06:21 +0530

    updated

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // Add before routes
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3001');
+    res.header('Access-Control-Allow-Origin', 'https://google-frontend-9d7i.onrender.com');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Credentials', 'true');
